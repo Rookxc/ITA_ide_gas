02d9f2a4c156c81e9df8a0b68e617203b85722e4
Rok Šekoranja <rok.sekoranja@tridenstechnology.com>
2024-03-11 18:17:33 +0100

    Initial commit

---
+++ src/main/java/com/example/idegas/IdegasApplication.java
@@ -0,0 +1,13 @@
+package com.example.idegas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdegasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdegasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/idegas/IdegasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.idegas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdegasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
