e92b3a3f3b00ce383e436fc49a5bce94eda92199
Rok Šekoranja <rok.sekoranja@tridenstechnology.com>
2024-03-12 09:32:27 +0100

    Added logger

--- src/main/java/com/example/idegas/service/CarAdService.java
+++ src/main/java/com/example/idegas/service/CarAdService.java
@@ -23,32 +23,37 @@ public class CarAdService {
 
     // Dodajanje novega avtomobilskega oglasa
     public CarAd addCarAd(CarAd carAd) {
-        logger.info("HELLO");
+        logger.info("addCarAd called");
         return carAdRepository.save(carAd);
     }
 
     // Pridobitev vseh avtomobilskih oglasov
     public List<CarAd> getAllCarAds() {
+        logger.info("getAllCarAds called");
         return carAdRepository.findAll();
     }
 
     // Pridobitev avtomobilskega oglasa po ID-ju
     public Optional<CarAd> getCarAdById(String id) {
+        logger.info("getCarAdById called");
         return carAdRepository.findById(id);
     }
 
     // Posodobitev avtomobilskega oglasa
     public CarAd updateCarAd(String id, CarAd updatedCarAd) {
+        logger.info("updateCarAd called");
         if (carAdRepository.existsById(id)) {
             updatedCarAd.setId(id);
             return carAdRepository.save(updatedCarAd);
         } else {
+            logger.error("CarAd with ID " + id + " not found.");
             throw new IllegalArgumentException("CarAd with ID " + id + " not found.");
         }
     }
 
     // Brisanje avtomobilskega oglasa
     public void deleteCarAd(String id) {
+        logger.info("deleteCarAd called");
         carAdRepository.deleteById(id);
     }
 }
